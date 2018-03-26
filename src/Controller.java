c01cb27461d4ab790bb83eb945a0466221804b53
ltybc138 <semenov200108@gmail.com>
2018-03-26 22:45:45 +0500

    Created form for logging in

--- src/Controller.java
+++ src/Controller.java
@@ -1,13 +1,29 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    public Button startButton;
-    public Label startedLabel;
 
-    public void onStartAction(ActionEvent actionEvent) {
-        startedLabel.setText("Started");
-        startButton.setDisable(true);
+    public Label loginLabel;
+    public TextField loginTextField;
+    public PasswordField passwordTextField;
+    public Button singinButton;
+    public Label checkerLabel;
+
+    public void signinAction(ActionEvent actionEvent) {
+        if (formValidation()) {
+            checkerLabel.setText("Correct");
+        } else {
+            checkerLabel.setText("Incorrect");
+        }
+    }
+
+    private boolean formValidation() {
+        if (loginTextField.getText().length() < 4 || passwordTextField.getText().length() < 8) {
+            return false;
+        }
+        return true;
     }
 }
