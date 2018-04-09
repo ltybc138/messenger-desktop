1b3514ab19facae55fb4e4fe69ed48e3c31ff6be
ltybc138 <semenov200108@gmail.com>
2018-04-09 23:42:50 +0500

    Register button in login window is kinda workable for now

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import utils.logging.LogType;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
@@ -10,6 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import utils.logging.Logger;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,8 +40,26 @@ public class LoginController implements Initializable {
     }
 
     public void registerAction(ActionEvent actionEvent) {
-        // TODO realise goto new register window
         logger.log("Register button tapped", LogType.ACTION);
+
+        // setting up loader
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../fxml/register.fxml"));
+        loader.setResources(resourceBundle);
+
+        try {
+            // setting up parent of the register scene
+            Parent fxmlMain = loader.load();
+            RegisterController controller = loader.getController();
+            controller.setMainStage(mainStage);
+            controller.setLogger(logger);
+
+            Scene registerScene = new Scene(fxmlMain, 280, 420);
+            mainStage.setScene(registerScene);
+            mainStage.show();
+        } catch (IOException e) {
+            logger.log(e.getMessage(), LogType.EXCEPTION);
+        }
     }
 
     /**
